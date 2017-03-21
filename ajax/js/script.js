bfe3c542b90e611273b74981b731669cab6638ac
anujsharma <sharma.anuj7996@gmail.com>
2017-03-21 20:28:25 +0530

    abc

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -1,21 +1,36 @@
 // Event handling
 document.addEventListener("DOMContentLoaded",
   function (event) {
+
+    var myname = document.querySelector("#data").value;
+    console.log(myname)
     
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
         
         // Call server to get the name
+        if (myname === anuj){
         $ajaxUtils
           .sendGetRequest("data/name.txt", 
             function (request) {
               var name = request.responseText;
 
               document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+                .innerHTML = "<h2>" + name + "!</h2>";
+            }); }
+
+          else if (myname === damini){
+              $ajaxUtils
+          .sendGetRequest("data/name2.txt", 
+            function (request) {
+              var name = request.responseText;
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + name + "!</h2>";
             });
 
+          }
         
       });
   }
