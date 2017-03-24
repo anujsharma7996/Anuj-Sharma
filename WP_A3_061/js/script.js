af77fb6dd624faba606b5d9bf40db63d4c9c5d24
anujsharma <sharma.anuj7996@gmail.com>
2017-03-24 10:25:51 +0530

    M2

---
+++ WP_A3_061/js/script.js
@@ -0,0 +1,41 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        var input =  document.querySelector("#nameis").value;
+       console.log(input);
+        // Call server to get the name
+
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+
+              if (input === res.name1) {
+              var message = res.name1;
+              var message2 = "Attendance of theory is " + res.att1t + "% , ";
+              var message3 = "Attendance of lab is " + res.att1l;
+              var message4 = "Theory and labs attended is 19/20 and 18/20 respectfully";
+                }
+
+                else if (input === res.name2) {
+
+              var message = res.name1;
+              var message2 = "Attendance of theory is " + res.att2t + "% , ";
+              var message3 = "Attendance of lab is " + res.att1l;
+              var message3 = "Theory and labs attended is 17/20 and 18/20 respectfully";
+                }
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+
+            });
+      });
+  }
+);
+
+
