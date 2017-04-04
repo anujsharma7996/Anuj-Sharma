bdd769aae8b04cb698e18fa3c51925dbf1e02891
anujsharma7996 <sharma.anuj7996@gmail.com>
2017-04-04 10:15:15 +0530

    Update script.js

--- m2_new/script.js
+++ m2_new/script.js
@@ -31,13 +31,7 @@ document.addEventListener("DOMContentLoaded",
                 }
 
               document.querySelector("#content")
-                .innerHTML = "<h2>" + message + "</h2>";
-          document.querySelector("#content")
-                .innerHTML = "<h2>" + message2 + "</h2>";
-          document.querySelector("#content")
-                .innerHTML = "<h2>" + message3 + "</h2>";
-          document.querySelector("#content")
-                .innerHTML = "<h2>" + message4 + "</h2>";
+                .innerHTML = message + "</br>" + message2 + "</br>" + message3 + "</br>"+ message4 + "</br>";
 
             });
       });
